b894fb611ad9073495fb79d03ccc7d830ac88b7c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-11 13:35:19 -0400

    Manage beforeinstallprompt

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,13 @@
+let addHomePromptEvent;
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js").then(() => {
         // console.log("Service worker registered.");
     });
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    addHomePromptEvent = event;
+    return false;
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,26 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+    "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+    createPostArea.style.display = "block";
+    if (addHomePromptEvent) {
+        addHomePromptEvent.prompt();
+        addHomePromptEvent.userChoice.then((result) => {
+            if (result.outcome === "dismissed") {
+            } else {
+            }
+        });
+        addHomePromptEvent = null;
+    }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+    createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
