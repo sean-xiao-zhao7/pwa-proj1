249643c924ee060f57e88ca4f93bcb36d115953e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-12 15:12:04 -0400

    Add images/fonts to pre-cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,12 +5,18 @@ self.addEventListener("install", (event) => {
             cache.addAll([
                 "/",
                 "/index.html",
+                "/manifest.json",
+                "/favicon.ico",
                 "/src/js/feed.js",
                 "/src/js/material.min.js",
                 "/src/js/app.js",
                 "/src/css/app.css",
                 "/src/css/feed.css",
                 "/src/css/help.css",
+                "/src/images/main-image.jpg",
+                "https://fonts.googleapis.com/css?family=Roboto:400,700",
+                "https://fonts.googleapis.com/icon?family=Material+Icons",
+                "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
             ]);
         })
     );
