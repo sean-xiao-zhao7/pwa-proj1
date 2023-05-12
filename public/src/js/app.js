4cf099678cbcbaafd7366bd9a521dcadca0b598d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-12 14:16:52 -0400

    Use fetch in SW

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 let addHomePromptEvent;
 
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js").then(() => {
         // console.log("Service worker registered.");
