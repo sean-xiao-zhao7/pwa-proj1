5316d153b20149a961a92d39e761ecc7fc86495f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-19 12:40:15 -0400

    Add firebase URL

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -9,29 +9,6 @@ var sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
     createPostArea.style.display = "block";
-    // if (deferredPrompt) {
-    //     deferredPrompt.prompt();
-
-    //     deferredPrompt.userChoice.then(function (choiceResult) {
-    //         console.log(choiceResult.outcome);
-
-    //         if (choiceResult.outcome === "dismissed") {
-    //             console.log("User cancelled installation");
-    //         } else {
-    //             console.log("User added to home screen");
-    //         }
-    //     });
-
-    //     deferredPrompt = null;
-    // }
-
-    // if ("serviceWorker" in navigator) {
-    //     navigator.serviceWorker
-    //         .getRegistration(serviceWorkerName)
-    //         .then((sw) => {
-    //             sw.unregister();
-    //         });
-    // }
 }
 
 function closeCreatePostModal() {
@@ -64,10 +41,10 @@ function createCard() {
     sharedMomentsArea.appendChild(cardWrapper);
 }
 
-const cardUrl = "https://httpbin.org/get";
+const cardUrl = "https://pwa2-e7438-default-rtdb.firebaseio.com/";
 let finished = false;
 
-fetch("https://httpbin.org/get")
+fetch(cardUrl)
     .then(function (res) {
         return res.json();
     })
