3f3181cfc7b3e57ddf409cb02ccec3747845012d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-01 14:03:24 -0400

    Add src/images to globPatterns

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{html,ico,json,js,css,png,jpg}'
-	],
-	swDest: 'public/workbox_sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+    globDirectory: "public/",
+    globPatterns: ["**/*.{html,ico,json,js,css}", "src/images/*.{jpg,png}"],
+    swDest: "public/workbox_sw.js",
+    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
